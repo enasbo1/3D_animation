5a25532b48192b59d651cb3dc2635162d4f9b688
Evan <evan.rktcisse@gmail.com>
2025-04-03 19:49:07 +0200

    Vector3D - sans __get_x & __set_x

--- backwork/Vector3D.py
+++ backwork/Vector3D.py
@@ -9,19 +9,6 @@ class Vector3D:
     def zero():
             return Vector3D(0, 0, 0)
 
-    @staticmethod
-    def from_exp(axis, angle:float):
-        """ crée un vecteur 3D à partir d'un axe de rotation (vecteur unitaire) et d'un angle en radians. utilise la rotation autour de l'axe donné """
-        cos_a = math.cos(angle)
-        sin_a = math.sin(angle)
-        x, y, z = axis.x, axis.y, axis.z
-        
-        return Vector3D(
-            cos_a + (1-cos_a)*x*x,
-            (1-cos_a)*x*y - sin_a*z,
-            (1-cos_a)*x*z + sin_a*y
-        )
-
     def __add__(self, other):
         return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
 
@@ -68,33 +55,6 @@ class Vector3D:
         
         fact = norm/self.__get_norm()
         self.__coord = (self.__get_x() * fact, self.__get_y() * fact, self.__get_z() * fact)
-
-    def __set_exp(self, exp:tuple["Vector3D", float]):
-        """
-            définit le vecteur en utilisant un axe de rotation et un angle.
-            exp = (axe, angle)
-        """
-        axis, angle = exp
-        new_vector = Vector3D.from_exp(axis, angle)
-        self.__coord = new_vector.coord
-
-    def __get_exp(self)->tuple["Vector3D", float]:
-        """
-            la forme exponentielle est un tuple(angle, norme);
-            /!\ set & get la forme exponentielle est à faire avec moderation
-
-            ici, __get_exp retourne un axe unitaire et l'angle associé avec une approximation basée sur le profuit scalaire
-        """
-        norm = self.__get_norm()
-        if norm == 0:
-            return Vector3D(1, 0, 0), 0 # axe x pris par défaut
-        
-        axis = Vector3D(self.x/norm, self.y/norm, self.z/norm)
-        reference = Vector3D(1, 0, 0) if abs(axis.x) < 0.99 else Vector3D(0, 1, 0) # si x proche de 1, on évite l'angle 0 en prenant (0, 1, 0) comme vecteur unitaire
-        dot_product = axis.dot(reference)
-
-        angle = math.acos(dot_product)
-        return axis, angle
     
     def dot(self, other)->float:
         """ produit scalaire entre deux vecteurs """
@@ -113,4 +73,3 @@ class Vector3D:
     y:float = property(__get_y, __set_y)
     z:float = property(__get_z, __set_z)
     norm:float = property(__get_norm, __set_norm)
-    exponential:tuple["Vector3D", float] = property(__get_exp, __set_exp)

b089725c223fadfb4289960b9538338fa8274f68
Ahmad MOULON <moulonahmad@gmail.com>
2025-05-16 11:36:32 +0200

    fix menu

--- scripts/Menu.py
+++ scripts/Menu.py
@@ -7,7 +7,7 @@ class Menu:
     @staticmethod
     def selectMode(pygIO: PygIO):
         boxPosX = -pygIO.width // 2
-        boxPosY = 200
+        boxPosY = pygIO.height // 2 - 300
         pygIO.draw_rect(boxPosX, boxPosY + 20, 500, 2000, "#000000")
         pygIO.draw_text(boxPosX + 20, boxPosY + 60, "Main Menu:", 30, "#FFFFFF", False)
         pygIO.draw_text(boxPosX + 20, boxPosY + 100, "0: Select Mesh and Reset", 30, "#FFFFFF", False)
@@ -16,10 +16,10 @@ class Menu:
     @staticmethod
     def selectMesh(pygIO: PygIO):
         boxPosX = -pygIO.width // 2
-        boxPosY = 200
+        boxPosY = pygIO.height // 2 - 300
         pygIO.draw_rect(boxPosX, boxPosY + 20, 500, 2000, "#000000")
         pygIO.draw_text(boxPosX + 20, boxPosY + 60, "Mesh to display:", 30, "#FFFFFF", False)
-        # pygIO.draw_text(boxPosX + 20, boxPosY + 100, "0: Pyramid", 30, "#FFFFFF", False)
+        pygIO.draw_text(boxPosX + 20, boxPosY + 100, "0: Pyramid", 30, "#FFFFFF", False)
         pygIO.draw_text(boxPosX + 20, boxPosY + 140, "1: Cube", 30, "#FFFFFF", False)
         pygIO.draw_text(boxPosX + 20, boxPosY + 180, "9: Return", 30, "#FFFFFF", False)
 
@@ -27,12 +27,12 @@ class Menu:
     @staticmethod
     def selectTreatment(pygIO: PygIO):
         boxPosX = -pygIO.width // 2
-        boxPosY = 200
+        boxPosY = pygIO.height // 2 - 300
         pygIO.draw_rect(boxPosX, boxPosY + 20, 500, 2000, "#000000")
         pygIO.draw_text(boxPosX + 20, boxPosY + 60, "Treatment to display:", 30, "#FFFFFF", False)
         pygIO.draw_text(boxPosX + 20, boxPosY + 100, "0: None", 30, "#FFFFFF", False)
         pygIO.draw_text(boxPosX + 20, boxPosY + 140, "1: Test treatment", 30, "#FFFFFF", False)
         pygIO.draw_text(boxPosX + 20, boxPosY + 180, "2: Matrice rotation", 30, "#FFFFFF", False)
         pygIO.draw_text(boxPosX + 20, boxPosY + 220, "3: Quaternion rotation", 30, "#FFFFFF", False)
-        pygIO.draw_text(boxPosX + 20, boxPosY + 300, "9: Return", 30, "#FFFFFF", False)
+        pygIO.draw_text(boxPosX + 20, boxPosY + 260, "9: Return", 30, "#FFFFFF", False)
         pass
